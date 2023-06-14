0fbb27052e9ad0787f7fe880aebb27988a26af9a
DavidJPLeRoux <lerouxdj21@gcc.edu>
2023-06-14 19:07:56 -0400

    Excel
    Setup all classes within Excel and their various methods, **Only Stub Methods**

--- src/Excel.java
+++ src/Excel.java
@@ -1,5 +1,84 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Queue;
 
 public class Excel {
+    private Excel(){}
 
+    public class csv {
+        private csv(){ }
+
+        public Collection<Collection<String>> read(String fileName){
+            /**
+             * ToDo: Code
+             */
+            return null;
+        }
+
+        public void write(String fileName, Collection<Collection<String>> data){
+            /**
+             * ToDo: Code
+             */
+        }
+
+        public void write(String fileName, Collection<String> data, String delimeter){
+            /**
+             * ToDo: Code
+             */
+        }
+    }
+
+    public class xls {
+        private xls(){}
+
+        public Collection<Collection<String>> read(String fileName){
+            /**
+             * ToDo: Code
+             */
+            return null;
+        }
+
+        public void write(String fileName, Collection<Collection<String>> data){
+            /**
+             * ToDo: Code
+             */
+        }
+
+        public void write(String fileName, Collection<String> data, String delimeter){
+            /**
+             * ToDo: Code
+             */
+        }
+    }
+
+    public class xlsx {
+        private xlsx(){}
+
+        public Collection<Collection<String>> read(String fileName){
+            /**
+             * ToDo: Code
+             */
+            return null;
+        }
+
+        public void write(String fileName, Collection<Collection<String>> data){
+            /**
+             * ToDo: Code
+             */
+        }
+
+        public void write(String fileName, Collection<String> data, String delimeter){
+            /**
+             * ToDo: Code
+             */
+        }
+    }
+
+    private Collection<Collection<String>> convert(Collection<String> data, String delimeter){
+        /**
+         * ToDo: Code
+         */
+        return null;
+    }
 
 }
