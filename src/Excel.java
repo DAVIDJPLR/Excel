47401b76f2c22db426f524aaf86a2655f40bef8f
DavidJPLeRoux <lerouxdj21@gcc.edu>
2023-06-14 21:05:08 -0400

    Excel.xls
    Attempted to code reading from xls

--- src/Excel.java
+++ src/Excel.java
@@ -3,6 +3,17 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
 public class Excel {
     private Excel(){}
 
@@ -81,6 +92,23 @@ public class Excel {
             /**
              * ToDo: Code
              */
+
+            HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(file));
+            HSSFSheet myExcelSheet = myExcelBook.getSheet("Birthdays");
+            HSSFRow row = myExcelSheet.getRow(0);
+
+            if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING){
+                String name = row.getCell(0).getStringCellValue();
+                System.out.println("name : " + name);
+            }
+
+            if(row.getCell(1).getCellType() == HSSFCell.CELL_TYPE_NUMERIC){
+                Date birthdate = row.getCell(1).getDateCellValue();
+                System.out.println("birthdate :" + birthdate);
+            }
+
+            myExcelBook.close();
+
             return null;
         }
 
